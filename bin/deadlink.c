53f75f623f635ab92676be85ecfd5dc078f970d0
Sean Marshallsay <srm.1708@gmail.com>
2013-12-23 22:24:22 +0000

    Command line options added. Option for recursive searching added.

--- bin/deadlink.c
+++ bin/deadlink.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <unistd.h>
-#include <sys/stat.h>
 #include <bstrlib/bstrlib.h>
 
 
 int crawl_dir(bstring dir_name);
-int file_exists(char* path_name);
 
 
-/* FILE->D_NAME DOES NOT RETURN A PATHNAME WHICH IS CAUSING THE ERROR */
+int rflag = 0;
+int vflag = 0;
+
+
 int main(int argc, char* argv[]) {
-    bstring b = bfromcstr("./tests/");
+    if (argc < 2) {
+        fprintf(stderr, "usage: deadlink [-rv] directory\n");
+        return -1;
+    }
+
+    int opt_ch;
+    do {
+        opt_ch = getopt(argc, argv, "rv");
+
+        if (opt_ch == -1) break;
+
+        switch (opt_ch) {
+            case 'r':
+                // follow directories
+                rflag = 1;
+                break;
+            case 'v':
+                // print names of files and directories before
+                // deleting or moving into them
+                vflag = 1;
+                break;
+        }
+    } while (opt_ch != -1);
+
+    bstring b = bfromcstr(argv[argc - 1]);
     crawl_dir(b);
     bdestroy(b);
     return 0;
@@ -20,9 +45,8 @@ int main(int argc, char* argv[]) {
 
 int crawl_dir(bstring dir_name) {
     // ensure dir_name ends in '/'
-    if (dir_name->data[dir_name->slen] != '/') bconchar(dir_name, '/');
+    if (dir_name->data[dir_name->slen - 1] != '/') bconchar(dir_name, '/');
 
-    fprintf(stderr, "\nEntering crawl_dir()!\n\n");
     // contains files in a directory
     DIR* dir = opendir(dir_name->data);
 
@@ -32,9 +56,12 @@ int crawl_dir(bstring dir_name) {
     if (dir == NULL) {
         fprintf(stderr, "Could not open directory\n\t%s\n", dir_name->data);
     } else {
-        file = readdir(dir);
+        do {
+            file = readdir(dir);
 
-        while (file != NULL) {
+            if (file == NULL) break;
+
+            // ignore current and parent directories
             if (strncmp(file->d_name, ".", 1) == 0 ||
                     strncmp(file->d_name, "..", 2) == 0) {
                 file = readdir(dir);
@@ -46,17 +73,15 @@ int crawl_dir(bstring dir_name) {
             bcatcstr(path, file->d_name);
 
             if (file->d_type == DT_DIR) {
-                printf("Directory: %s\n", file->d_name);
+                printf("Directory: %s\n", path->data);
 
-                crawl_dir(path);
+                if (rflag) crawl_dir(path);
             } else if (file->d_type == DT_LNK) {
                 // access will return -1 if link points to nothin
                 int link_is_dead = access(path->data, F_OK) == -1;
-                if (link_is_dead) printf("Dead Link: %s\n", file->d_name);
+                if (link_is_dead) printf("Dead Link: %s\n", path->data);
             }
-
-            file = readdir(dir);
-        }
+        } while (file != NULL);
 
         closedir(dir);
     }
--- tests/runtests.sh
+++ tests/runtests.sh
@@ -2,7 +2,44 @@
 
 echo ""
 
-echo "Running bin/deadlink on tests/"
-bin/deadlink
+
+echo "Creating test directories."
+
+cd tests
+
+mkdir test_directory
+cd test_directory
+
+touch level_one_file
+ln -s level_one_file level_one_link
+ln -s level_one_fake level_one_dead
+
+mkdir directory
+cd directory
+
+touch level_two_file
+ln -s level_two_file level_two_link
+ln -s level_two_fake level_two_dead
+
+cd ../..
+
+
+echo ""
+echo "Running without arguments:"
+../bin/deadlink
+
+echo ""
+echo "Running without -r:"
+../bin/deadlink test_directory
+
+echo ""
+echo "Running with -r:"
+../bin/deadlink -r test_directory
+
+
+echo ""
+echo "Removing test directories."
+rm -rf test_directory
+
 
 echo ""
